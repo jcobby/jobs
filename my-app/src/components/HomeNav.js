251c9d44e4595b1b80e897c9d55f081864cd13fd
jcobby <justicecobbinah19@gmail.com>
2023-07-12 20:27:35 +0000

    mobile resp

--- my-app/src/components/HomeNav.js
+++ my-app/src/components/HomeNav.js
@@ -13,12 +13,14 @@ import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
 import "./HomeNav.css";
 import search from "../images/icon-search.svg";
 import location from "../images/icon-location.svg";
-import location_filter from '../assets/mobile/icon-filter.svg'
+import location_filter from "../assets/mobile/icon-filter.svg";
 import check from "../images/icon-check.svg";
 import det from "../SomeDetails";
 
 function HomeNav() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
 
   useEffect(() => {
     const handleResize = () => {
@@ -32,6 +34,15 @@ function HomeNav() {
       window.removeEventListener("resize", handleResize);
     };
   }, []); // Empty dependency array to run the effect only once
+  
+  const handleLocationFilterClick = () => {
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
+console.log('the popin' + isPopupOpen)
 
   return (
     <div>
@@ -113,8 +124,59 @@ function HomeNav() {
             type="text"
             placeholder="Filter by location..."
           />
-          <img className="location-mobile" src={location_filter}></img>
-          <button className="button-search"><img className="" style={{ filter: 'brightness(0) invert(1)' }} src={search}></img></button>
+          <button className="location-mobile" onClick={handleLocationFilterClick}>
+          <img    src={location_filter} ></img>
+
+          </button>
+          <button className="button-search"  onClick={handleLocationFilterClick}>
+            <img
+              className=""
+              style={{ filter: "brightness(0) invert(1)" }}
+              src={search}
+            ></img>
+          </button>
+
+          {isPopupOpen && (
+            <div className="popup-container" >
+              <div className="popup">
+                {/* Your pop-up content goes here */}
+                {/* <div className="mobile-nav"> */}
+          <input
+            className="pop-up-input-location"
+            type="text"
+            placeholder="Filter by location..."
+          />
+           <img className="pop-up-location" src={location}></img>
+          {/* </div> */}
+                {/* <label htmlFor="full-time-checkbox">
+                  <input
+                    type="checkbox"
+                    id="full-time-checkbox"
+                    name="full-time-checkbox"
+                  />
+                  Full Time Only
+                </label> */}
+                 <div
+                  className="pop-up-input-check"
+                  type="none"
+                  style={{
+                    // width: '10000px',
+                    border: "none",
+                    borderBottomRightRadius: "10px",
+                    borderTopRightRadius: "10px",
+                  }}
+                >
+                  <h4 className="h4-foot">Full Time Only</h4>
+                </div>
+                <label class="checkbox-container">
+                <input type="checkbox" />
+                <span class="checkmark"></span>
+              </label>
+                <button className="pop-up-button-search" onClick={handleClosePopup}>Apply</button>
+              </div>
+            </div>
+          )}
+
         </div>
       )}
     </div>
